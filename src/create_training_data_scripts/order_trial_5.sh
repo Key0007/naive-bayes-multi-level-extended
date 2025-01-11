cf4ba25191105dff36e905ddc09684197268daf3
key-r-code <kramjattun@gmail.com>
2025-01-10 20:34:28 -0500

    training_data scripts

---
+++ src/create_training_data_scripts/order_trial_5.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+### select the partition "def"
+#SBATCH --partition=def
+### set email address for sending job status
+#SBATCH --mail-user=kr3288@drexel.edu
+### account - essentially your research group
+#SBATCH --account=rosenMRIPrj
+### select number of nodes
+#SBATCH --nodes=1
+### select number of tasks per node (threads)
+#SBATCH --ntasks-per-node=48
+### request 2 hours of wall clock time
+#SBATCH --time=12:00:00
+### memory size required per node (memory)
+#SBATCH --mem=185GB
+#SBATCH --cpus-per-task=1
+
+. ~/.bashrc
+start=`date +%s`
+
+conda run -n myenv python3 everything_extended1.py order trial_5
+
+end=`date +%s`
+runtime=$((end-start))
+echo "run time: $runtime"
