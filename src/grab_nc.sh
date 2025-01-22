0305ea4456a9b76ac7f44e350026f6673e3fa5ed
key-r-code <kramjattun@gmail.com>
2025-01-22 11:33:16 -0500

    modified grab_nc to get first field

--- src/grab_nc.sh
+++ src/grab_nc.sh
@@ -6,7 +6,7 @@ for taxa_ in "${taxa[@]}"; do
 	   for trial_ in "${trials[@]}"; do
 		   cd /ifs/groups/rosenMRIGrp/kr3288/extended/${taxa_}_testing/15-mers/training_data/${trial_}/
            find . -type f -name "*.fna" | while read file; do
-                grep '>N' $file >> /ifs/groups/rosenMRIGrp/kr3288/extended/training_lists/${taxa_}_${trial_}.txt
+                grep -o '^>N[^[:space:]]*' $file >> /ifs/groups/rosenMRIGrp/kr3288/extended/training_lists/${taxa_}_${trial_}.txt
             done
 	done
 done
