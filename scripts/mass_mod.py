96b1306ab2aa66419be250346ccedd02a73c3c8f
key-r-code <kramjattun@gmail.com>
2025-02-01 16:16:04 -0500

    added mp

--- scripts/mass_mod.py
+++ scripts/mass_mod.py
@@ -1,5 +1,4 @@
 import os
-import re
 import json
 import polars as pl
 from multiprocessing import Pool
@@ -29,34 +28,31 @@ def create_trial_map(taxa):
 
 def create_lookup(taxa):
     return (
-        pl.read_csv('/Users/key/extended/new_extended_lineage.csv')
+        pl.read_csv('/ifs/groups/rosenMRIGrp/kr3288/extended/new_extended_lineage.csv')
         .fill_null('')
         .with_columns(
-            pl.col(pl.Utf8).str.strip_chars()
+            pl.col(pl.Utf8).str.strip_chars(),
+            pl.col('Species_ID').cast(str)
         )
         .select(['Species_ID', taxa])
         .unique()
     )
 
-def output_modifier(csv_file_path):
 
 def output_modifier(csv_file_path):
-    # Extract taxa and set up paths/mappings
+
     taxa = csv_file_path.split('/')[-1].split("_")[2]
     TRIAL_MAP = create_trial_map(taxa)
+    trial_number = csv_file_path.split("/")[-1].split("_")[1]
     lookup = create_lookup(taxa.capitalize())
-    
-    # Load JSON dictionary
+
     with open('/ifs/groups/rosenMRIGrp/kr3288/eeee.json', 'r') as j:
         dict_ = json.load(j)
-    
     l = {value: key for key, values in dict_.items() for value in values}
-    
-    # Create directory
+
     mod_path = f"/ifs/groups/rosenMRIGrp/kr3288/extended/{taxa}_modified"
     os.makedirs(mod_path, exist_ok=True)
 
-    # Read and process CSV
     df = (
         pl.read_csv(
             csv_file_path,
@@ -69,7 +65,6 @@ def output_modifier(csv_file_path):
         ])
     )
 
-    # First merge operation
     df = (
         df.join(
             lookup.select(['Species_ID', pl.col(taxa.capitalize())]),
@@ -77,21 +72,16 @@ def output_modifier(csv_file_path):
             right_on='Species_ID',
             how='left'
         )
-        .drop('Species_ID')
         .rename({taxa.capitalize(): f'Predicted {taxa.capitalize()}'})
     )
 
-    # Add actual species
-    df = (
-        df.with_columns([
-            pl.col('NCBI RefSeq').map_dict(l, default='').alias('Actual Species'),
-        ])
-        .with_columns([
-            pl.col('Actual Species').str.strip_chars()
-        ])
+    df = df.with_columns(
+        pl.col('NCBI RefSeq')
+        .replace_strict(l, default='')
+        .str.strip_chars()
+        .alias('Actual Species')
     )
 
-    # Second merge operation
     df = (
         df.join(
             lookup.select(['Species_ID', pl.col(taxa.capitalize())]),
@@ -99,11 +89,9 @@ def output_modifier(csv_file_path):
             right_on='Species_ID',
             how='left'
         )
-        .drop('Species_ID')
         .rename({taxa.capitalize(): f'Actual {taxa.capitalize()}'})
     )
 
-    # Process NCBI RefSeq and add Known/Unknown
     df = (
         df.with_columns([
             pl.col('NCBI RefSeq')
@@ -114,37 +102,27 @@ def output_modifier(csv_file_path):
         ])
         .with_columns([
             pl.col('NCBI RefSeq striped')
-                .map_dict({k: 'Known' for k in TRIAL_MAP.get(trial_number, '')}, default='Unknown')
+                .replace_strict({k: 'Known' for k in TRIAL_MAP.get(trial_number, '')}, default='Unknown')
                 .alias('Known/Unknown')
         ])
     )
 
-    # Add accuracy column and final processing
-    df = (
-        df.with_columns([
-            (pl.col(f"Predicted {taxa}") == pl.col(f"Actual {taxa}"))
-                .alias('Accurate Prediction')
-        ])
-        .drop('NCBI RefSeq striped')
-    )
-
-    # Save output
     output_filename = os.path.basename(csv_file_path)
     df.write_csv(f'{mod_path}/mod_{output_filename}')
 
 
 if __name__ == "__main__":
 
-    # all_csv_paths = []
+    all_csv_paths = []
 
-    # for trial in ['trial_1', 'trial_2', 'trial_3', 'trial_4', 'trial_5']:
-    #     for kmer in ['3', '6', '9', '12', '15']:
-    #         for taxa in ['phylum', 'class', 'order', 'family']:
-    #             path = os.path.join(f'/ifs/groups/rosenMRIGrp/kr3288/extended/{taxa}_testing/{kmer}-mers/classification_results', f'{trial}_{taxa}_{kmer}mers.csv')
-    #             all_csv_paths.append(path)
+    for trial in ['trial_1', 'trial_2', 'trial_3', 'trial_4', 'trial_5']:
+        for kmer in ['3', '6', '9', '12', '15']:
+            for taxa in ['phylum', 'class', 'order', 'family']:
+                path = os.path.join(f'/ifs/groups/rosenMRIGrp/kr3288/extended/{taxa}_testing/{kmer}-mers/classification_results', f'{trial}_{taxa}_{kmer}mers.csv')
+                all_csv_paths.append(path)
 
 
-    # num_threads = int(os.environ.get('SLURM_NTASKS', 48))
+    num_threads = int(os.environ.get('SLURM_NTASKS', 48))
     
-    # with Pool(processes=num_threads) as pool:
-    #     pool.map(output_modifier, all_csv_paths)
+    with Pool(processes=num_threads) as pool:
+        pool.map(output_modifier, all_csv_paths)
